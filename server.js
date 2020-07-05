e8373119bae57f7b708eb3f56c9788d33c8ab586
Ivan Duran <idurandiaz@gmail.com>
2020-07-06 01:37:15 +0300

    Install cors to allow for cross-origin

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('corsc');
 const schema = require('./schema');
 
 const app = express();
+
+// allow cross-origin
+app.use(cors());
 
 app.use(
   '/graphql',
@@ -15,4 +19,3 @@ app.use(
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-

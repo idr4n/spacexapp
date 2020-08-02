1d52e9ad9d21ef5f68aa516fea5afcde0ccac7de
Ivan Duran <idurandiaz@gmail.com>
2020-08-02 09:28:22 +0300

    Add legend for launch sucess or fail

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MissionKey = () => {
+  return (
+    <div className="my-3">
+      <p>
+        <span className="px-3 mr-2 bg-success" /> = Sucess
+      </p>
+      <p>
+        <span className="px-3 mr-2 bg-danger" /> = Fail
+      </p>
+    </div>
+  );
+};
+
+export default MissionKey;

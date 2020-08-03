b454f0a5038b226ffe577803febf4a81872314ee
Ivan Duran <idurandiaz@gmail.com>
2020-08-03 15:14:51 +0300

    Build into public folder in server app

---
+++ public/precache-manifest.baf93b0f645f968779261f809c4124de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4f3d748338e136298b5be9f30035c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d9e708872ce3c19338",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc6a5dd80e8499edd3aa",
+    "url": "/static/js/2.b11cd473.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.b11cd473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d9e708872ce3c19338",
+    "url": "/static/js/main.01f1a96b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "03637b0f9a0c962dc5d3f86ae6f86f46",
+    "url": "/static/media/logo.03637b0f.svg"
+  }
+]);
